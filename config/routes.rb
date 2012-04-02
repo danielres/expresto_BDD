5170e4493350e9d2df17923f35691d2f089fcb20
Daniel Reszka <draens@hotmail.com>
2012-04-02 20:48:42 +0200

    added route 'resources :users'

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Expresto::Application.routes.draw do
+  resources  :users
   devise_for :users
 
   authenticated :user do
     root :to => 'home#index'
   end
 
-  
+
   root :to => "home#index"
 
 end
