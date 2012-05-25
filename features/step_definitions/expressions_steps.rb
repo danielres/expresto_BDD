a980710351eaa96d98c66a62e8323643285b49ef
Daniel Reszka <draens@hotmail.com>
2012-05-25 15:30:54 +0200

    expressions, homepage: implemented link list on homepage with BDD coverage

--- features/step_definitions/expressions_steps.rb
+++ features/step_definitions/expressions_steps.rb
@@ -4,11 +4,10 @@ Given /^(\d+) (.*) expressions have been added$/ do |amount, language|
   end
 end
 
-Then /^I should see (\d+) expressions in (.*)$/ do |amount, language|
-  find('.expressions').should have_xpath(
-    "*[@class='expression' and @lang='#{language.downcase[0..1]}']",
+Then /^I should see a link list with (\d+) expressions in (.*)$/ do |amount, language|
+  code = language.downcase[0..1]
+  find('ul.expressions').should have_xpath(
+    "li[@class='expression' and @lang='#{code}']//a[contains(@href,'/#{code}/expressions/')]",
     :count => amount
   )
 end
-
-
