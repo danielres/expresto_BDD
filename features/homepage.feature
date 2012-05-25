87e5123b85d7fcfa2efcc06c57afb64ea7ad0e7f
Daniel Reszka <draens@hotmail.com>
2012-05-25 03:17:16 +0200

    cucumber: better step definitions for expressions in different languages

--- features/homepage.feature
+++ features/homepage.feature
@@ -9,6 +9,7 @@ Feature: homepage
 
   Scenario: See most recent expressions on homepage
     Given 2 english expressions have been added
+    And   3 french  expressions have been added
     When I go to the homepage
     Then I should see language menu with English, French
     Then I should see "recent expressions"
--- features/step_definitions/expressions_steps.rb
+++ features/step_definitions/expressions_steps.rb
@@ -1,6 +1,6 @@
-Given /^(\d+) english expressions have been added$/ do |arg1|
-  arg1.to_i.times do |n|
-    FactoryGirl.create(:expression)
+Given /^(\d+) (.*) expressions have been added$/ do |amount, language|
+  amount.to_i.times do |n|
+    FactoryGirl.create(:expression, :language => Language.find_by_code(language[0..1].downcase) )
   end
 end
 
