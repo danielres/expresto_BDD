c2621f2b1ad4cd5e81551efc05e3d48c65357261
Daniel Reszka <draens@hotmail.com>
2012-05-25 02:56:08 +0200

    languages, layout: implemented languages menu into app layout, TDD-style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :load_available_languages
+
+private
+
+  def load_available_languages
+    @available_languages = Language.all
+  end
+
 end
