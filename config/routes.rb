139fb2876a7450c9e8d915854b4d0c14aca47231
Daniel Reszka <draens@hotmail.com>
2012-06-12 18:23:16 +0200

    pages, about: created page 'about' with its contents in french & in english

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Expresto::Application.routes.draw do
   root :to => "home#redirect_to_locale"
 
   scope "(:locale)" do
+    match 'about' => 'pages#about', :as => :about
     root :to => "home#index"
     devise_for  :users
     resources   :users
     resources   :languages
     resources   :expressions
+
   end
 
 end
