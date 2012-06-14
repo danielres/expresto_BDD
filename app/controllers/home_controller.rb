c62232edb6c854702d9ce9502f2bb2d29bcfc92c
Daniel Reszka <draens@hotmail.com>
2012-06-14 14:33:55 +0200

    FIX: rescue from unavailable locale in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,19 @@
 class HomeController < ApplicationController
+
+  rescue_from RuntimeError, :with => :rescue_from_unavailable_locale #FIXME: hmmm... not sure it's the best way to handle this
+
   def index
     @expressions = Expression.in(params[:locale]).recent(10)
   end
+
   def redirect_to_locale
     redirect_to "/#{locale}"
   end
+
+private
+
+  def rescue_from_unavailable_locale
+    redirect_to "/#{Language.first.code}", :notice => "Sorry, but the locale \"#{locale.upcase}\" is not implemented yet, so you have been redirected here."
+  end
+
 end
