1d846cad11b5ccdf8d48d5685e7be7618ccac5e4
Daniel Reszka <draens@hotmail.com>
2012-06-15 02:40:53 +0200

    news, advanced_format: implemented dynamic user_id replacement based on special syntax, in news

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,7 @@ module ApplicationHelper
     attributes[:class] = attributes[:class].to_s + ' todo'
     content_tag(wrapper, capture(&block), attributes) if ( Rails.env == 'development' )
   end
+  def advanced_format(*args)
+    simple_format(*args).gsub(/@(\d+)/) { |s| u=User.find(s[1..-1].to_i); link_to u, u }.html_safe
+  end
 end
