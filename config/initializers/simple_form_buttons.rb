e2b6720591a55ed88f606782c353aaf76f066431
Daniel Reszka <dannn.r@gmail.com>
2013-03-10 20:08:36 +0100

    view cleanup

--- config/initializers/simple_form_buttons.rb
+++ config/initializers/simple_form_buttons.rb
@@ -1,15 +1,15 @@
 module SimpleForm
   class FormBuilder < ActionView::Helpers::FormBuilder
 
-    def submit_button(title=nil, options={}, &block)
+    def submit_button title=nil, options={}, &block
       options = {
           type: 'submit',
           class: 'btn btn-primary',
           data:{ purpose: 'submit-button' }
-      }.merge(options)
-      title, options = nil, title if title.is_a?(Hash)
+      }.merge options
+      title, options = nil, title if title.is_a? Hash
       title ||= submit_default_value
-      @template.content_tag(:button, title, options)
+      @template.content_tag :button, title, options
     end
 
   end
